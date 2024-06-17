a7ed133c1df5afeab492084bcda85c8febaae585
ilboued <ilboued@proton.me>
2024-06-17 15:18:26 +0200

    add cors to accept all origins

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import cors from 'cors';
+
 import playersync  from 'playersync';
 console.log('💫🚀 playersync started...');
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+    origin: '*', // Accepter les requêtes de n'importe quelle origine
+    optionsSuccessStatus: 200 
+}));
 
 playersync(app);
 
